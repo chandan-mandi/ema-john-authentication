ad0870c3448abac5468d9c118066c4417cf8b801
chandan-mandi <itscoderchandan@gmail.com>
2021-10-12 13:35:18 +0530

    auth config added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAXn_ffY5e83VM4sGZuYz-QWAgapg0VVoU",
+    authDomain: "ema-john-simple-17cc8.firebaseapp.com",
+    projectId: "ema-john-simple-17cc8",
+    storageBucket: "ema-john-simple-17cc8.appspot.com",
+    messagingSenderId: "442431268326",
+    appId: "1:442431268326:web:df4b2d48a72d8baa262227"
+};
+
+export default firebaseConfig;
